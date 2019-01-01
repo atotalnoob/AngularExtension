f63e528a8107aa6b6b1030d7b62497cdc421144f
atotalnoob <atotalnoob@users.noreply.github.com>
2019-01-01 14:05:10 -0500

    Basic layout

---
+++ AngularExtension/Intellisense/AngularHTMLIntellisenseProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+
+namespace AngularExtension.Intellisense
+{
+    [Export(typeof(IIntellisensePresenterProvider))]
+    [ContentType("HTML")]
+    [Order(Before = "default")]
+    [Name("Angular HTML Code Snippets")]
+    class AngularHTMLIntellisenseProvider : IIntellisensePresenterProvider
+    {
+        public IIntellisensePresenter
+          TryCreateIntellisensePresenter(IIntellisenseSession session)
+        {
+
+            // returning null will 
+            // trigger the default Intellisense provider
+            return null;
+        }
+    }
+}
---
+++ AngularExtension/Intellisense/AngularTypeScriptIntellisenseProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+
+namespace AngularExtension.Intellisense
+{
+    [Export(typeof(IIntellisensePresenterProvider))]
+    [ContentType("TypeScript")]
+    [Order(Before = "default")]
+    [Name("Angular TyypeScript Code Snippets")]
+    class AngularTypeScriptIntellisenseProvider : IIntellisensePresenterProvider
+    {
+        public IIntellisensePresenter
+           TryCreateIntellisensePresenter(IIntellisenseSession session)
+        {
+
+            // returning null will 
+            // trigger the default Intellisense provider
+            return null;
+        }
+    }
+}
